3a81fe6971e07b13185f5fc13a218cb569f00248
ChenPingSheng <ysuchx@sina.com>
2015-12-23 02:38:00 +0800

    Modify comment

--- ApprovalNumberInputViewDemo/ApprovalNumberInputViewDemo/ApprovalNumberInputView/ApprovalNumberInputView.h
+++ ApprovalNumberInputViewDemo/ApprovalNumberInputViewDemo/ApprovalNumberInputView/ApprovalNumberInputView.h
@@ -1,9 +1,9 @@
 //
 //  ApprovalNumberInputView.h
-//  WYYSS
+//  
 //
 //  Created by chenxin on 15/12/22.
-//  Copyright © 2015年 WYYSS. All rights reserved.
+//  Copyright © 2015年 ChenXinApp. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- ApprovalNumberInputViewDemo/ApprovalNumberInputViewDemo/ApprovalNumberInputView/ApprovalNumberInputView.m
+++ ApprovalNumberInputViewDemo/ApprovalNumberInputViewDemo/ApprovalNumberInputView/ApprovalNumberInputView.m
@@ -1,9 +1,9 @@
 //
 //  ApprovalNumberInputView.m
-//  WYYSS
+//  
 //
 //  Created by chenxin on 15/12/22.
-//  Copyright © 2015年 WYYSS. All rights reserved.
+//  Copyright © 2015年 ChenXinApp. All rights reserved.
 //
 
 #import "ApprovalNumberInputView.h"
